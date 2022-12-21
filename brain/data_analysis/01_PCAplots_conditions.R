afc42b3fa78c914be16cf049cc26df7a369784d5
ty14 <milewski@utexas.edu>
2022-12-21 16:14:51 -0600

    please

--- brain/data_analysis/01_PCAplots_conditions.R
+++ brain/data_analysis/01_PCAplots_conditions.R
@@ -199,7 +199,115 @@ ggplot(d1, aes(PC2, PC4, color = condition1, shape = condition1))+
 
 #########
 # subs
+# Expression values
+dlNorm <-  read.csv("brain/PFC_counts.csv", row.names = 1)
+#remove zeros - Becca told me to do this since zeros can fuck things up. 
+dlNorm <- dlNorm[apply(dlNorm[], 1, function(x) !all(x==0)),]
 
+#Lines 20 to 70 is just me getting my group variables together
+#trim sample ids
+colnames(dlNorm)[c(1:67)] <- substr(colnames(dlNorm)[c(1:67)], 7, 13)
+
+#Group traits
+#Getting metadata ready 
+coldata <- read_csv("brain/sample70min_table.csv")
+head(coldata)
+str(coldata)
+
+#fixing things
+coldata$region <- gsub("mPF", "PFC", coldata$region)
+coldata$groupEX <- coldata$group
+
+
+# Normalizing cort data
+# df <- transform(df, N = (N - min(N)) / (max(N) - min(N))
+
+coldata <-coldata %>% mutate(post_Ncort =  (mean_con_ng_ul - min(mean_con_ng_ul,na.rm=T))/(max(mean_con_ng_ul,na.rm=T)-min(mean_con_ng_ul,na.rm=T)))
+coldata <- coldata %>%  dplyr::select(-group, -period)
+
+#getting condition1
+table(coldata$condition)
+# CDOM, RDOM to Descenders (DOM to SUB)(4->1)
+# CSUB, SUB to Ascenders (Sub to DOM)  (1->4)
+
+coldata$condition1 <- ifelse(coldata$condition == "same" & coldata$Prerank == 1, "DOM", coldata$condition)
+coldata$condition1 <- ifelse(coldata$condition1 == "same" & coldata$Prerank == 4, "SUB", coldata$condition1)
+coldata$condition1 <- ifelse(coldata$condition == "descenders" & coldata$Postrank == 4 & coldata$Prerank == 1, "DES", coldata$condition1)
+coldata$condition1 <- ifelse(coldata$condition == "ascenders" & coldata$Prerank == 4 & coldata$Postrank == 1, "ASC", coldata$condition1)
+coldata$condition1 <- ifelse(coldata$condition == "control" & coldata$Postrank == 4, "CSUB", coldata$condition1)
+coldata$condition1 <- ifelse(coldata$condition == "control" & coldata$Postrank == 1, "CDOM", coldata$condition1)
+
+
+#just get samples I want
+coldata$SampleID <- substr(coldata$SampleName, 7, 13)
+
+coldata <- coldata %>%  dplyr::select(-SampleName) %>% 
+  filter(Postrank != 3) %>% 
+  filter(condition1 != 'ascenders') %>% 
+  pivot_wider(
+    names_from = region, 
+    values_from = region)
+
+row.names <- coldata$SampleID
+row.names(coldata) <- row.names #Assigning row names from as sample names  
+head(coldata)
+
+#check before normalizing 
+dlNorm<- dlNorm[, rownames(coldata)]
+all(rownames(coldata) == colnames(dlNorm))
+
+#normalize and filter with all groups -
+# I normalize with all groups since I want to compare them after
+
+dlNorm <- dlNorm[!is.na(rowSums(dlNorm)),]
+
+d = apply(dlNorm, 2, as.numeric)
+dim(d)
+
+d0= DGEList(d, group = coldata$condition1)
+dim(d0)
+rownames(d0) <- rownames(dlNorm)
+d0 <- calcNormFactors(d0)
+
+cutoff <-50
+drop <- which(apply(cpm(d0), 1, max) < cutoff)
+dge.dl <- d0[-drop,]
+dim(dge.dl)
+#6571   40
+
+# Now take out groups that you want
+#DOMs first 
+dge.dl$samples$group
+
+dge.dl_dom <- dge.dl[, dge.dl$samples$group %in% c("CSUB", "SUB", "ASC")]
+dge.dl_dom$samples$group <- droplevels(dge.dl_dom$samples$group)
+dge.dl_dom$samples$group
+dge.dl<- dge.dl_dom
+dge.dl$samples$group
+
+coldata %>% 
+  filter(condition1 != "DES") %>%
+  filter(condition1 != "DOM") %>%
+  filter(condition1 != "CDOM") %>%
+  dplyr::select(SampleID, condition1) -> var_info  
+
+row.names <- var_info$SampleID
+
+row.names(var_info) <- row.names #Assigning row names from as sample names  
+head(var_info )
+
+dlNorm<- dlNorm[, rownames(var_info)]
+all(rownames(var_info) == colnames(dlNorm)) #check
+
+##following Won's code
+var_info$condition1 %>%
+  factor(.,levels = c("CSUB","SUB","ASC")) -> group.dl
+
+
+design.dl <- model.matrix(~ 0 + group.dl)
+colnames(design.dl) -> mycolnames
+
+v.dl = voom(dge.dl, design.dl, plot = F)
 
 
 
